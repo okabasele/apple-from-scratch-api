42cd22939def82252e4ff40fc698cf47e84e4118
okabasele <ocekabasele@gmail.com>
2024-06-17 12:43:47 +0200

    chore: Update npm dependencies and enable SSL for production environment

--- dist/config/config.js
+++ dist/config/config.js
@@ -1,4 +1,15 @@
 "use strict";
+var __assign = (this && this.__assign) || function () {
+    __assign = Object.assign || function(t) {
+        for (var s, i = 1, n = arguments.length; i < n; i++) {
+            s = arguments[i];
+            for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
+                t[p] = s[p];
+        }
+        return t;
+    };
+    return __assign.apply(this, arguments);
+};
 var dotenv = require('dotenv');
 // On récupère l'environement dans lequel on se trouve (développement, production, test)
 var environment = process.env.NODE_ENV || 'development';
@@ -31,13 +42,10 @@ var commonConfig = {
     host: DB_HOST,
     port: DB_PORT,
     dialect: DB_DIALECT,
-    define: {
-        timestamps: false
-    }
 };
 // Export de la configuration
 module.exports = {
-    development: commonConfig,
+    development: __assign(__assign({}, commonConfig), { ssl: true }),
     test: commonConfig,
-    production: commonConfig
+    production: __assign(__assign({}, commonConfig), { ssl: true })
 };
